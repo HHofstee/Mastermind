69727670b3d8f8ebd4f8649ee55e393c276e44ba
Harm Hofstee <harm@tallcomponents.com>
2016-04-25 23:00:56 +0200

    permuating hussled list of colors
    Now we can shuffle color list
    We can make the permutations of 4 shuffle colorlists

---
+++ ArrayShuffler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastermind
+{
+    class ArrayShuffler<T>
+    {
+        public static T[] Shuffle(T[] a)
+        {
+            new Random().Shuffle<T>(a);
+            return a;
+        }
+    }
+}
---
+++ ColorlistPermutations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Mastermind
+{
+    class ColorlistPermutations
+    {
+        public ColorlistPermutations(Color[] all)
+        {
+            all_colors = all;
+        }
+
+        public IEnumerable<Color[]> GetColorlists()
+        {
+            foreach (var i in ArrayShuffler<Color>.Shuffle(all_colors))
+                foreach (var j in ArrayShuffler<Color>.Shuffle(all_colors))
+                    foreach (var k in ArrayShuffler<Color>.Shuffle(all_colors))
+                        foreach (var l in ArrayShuffler<Color>.Shuffle(all_colors))
+                            yield return new Color[] { i, j, k, l };
+        }
+
+        private Color[] all_colors;
+    }
+}
